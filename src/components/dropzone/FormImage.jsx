e3e32661cb94b726f6c6bdcca41696147e2e5179
ValeHernandezz <valehernandezmodino@gmail.com>
2024-10-14 19:02:02 -0300

    Style: Cambio de estilo del dropdown

--- src/components/dropzone/FormImage.jsx
+++ src/components/dropzone/FormImage.jsx
@@ -3,6 +3,8 @@ import useEditor from '@/store/Providers'
 import { useDropzone } from 'react-dropzone'
 import { useRouter } from 'next/navigation'
 
+import Paperclip from '@/components/icons/PaperClip'
+
 export default function FormImage() {
   const { changeImage } = useEditor()
   const router = useRouter()
@@ -10,11 +12,11 @@ export default function FormImage() {
   const { getRootProps, getInputProps, open } = useDropzone({
     noClick: true,
     accept: {
-      'image/*': [],
+      'image/*': []
     },
     onDrop: (acceptedFiles) => {
       handleDrop(acceptedFiles)
-    },
+    }
   })
 
   const handleDrop = async (acceptedFiles) => {
@@ -26,7 +28,7 @@ export default function FormImage() {
 
     const response = await fetch('/api/upload', {
       method: 'POST',
-      body: formData,
+      body: formData
     })
 
     const { data } = await response.json()
@@ -36,45 +38,19 @@ export default function FormImage() {
   }
 
   return (
-    <section
-      {...getRootProps({ className: 'dropzone' })}
-      className='card flex flex-col p-10'
-    >
-      <div>
-        <h2
-          className='text-center px-2 text-xl lg:text-2xl font-black text-white uppercase pb-5 sombra'
-          style={{ lineHeight: '1.2', wordWrap: 'break-word' }}
-        >
-          {/*    <span className='block px-4'>
-            <>
-              <span className='block'>
-                ¡Sube una
-                <span className='bg-gradient-to-r text-transparent from-blue-500 to-violet-600 bg-clip-text'>
-                  imagen
-                </span>
-              </span>
-              y comienza a
-              <span className='bg-gradient-to-r text-transparent from-blue-500 to-violet-600 bg-clip-text'>
-                personalizarla
-              </span>
-              !
-            </>
-          </span> */}
-        </h2>
+    <section {...getRootProps({ className: 'dropzone' })} className="card">
+      <div className="text">
+        <span className="mb-5">¡Sube una imagen!</span>
+        <p className="subtitle">O arrástrala</p>
       </div>
-
-      <div>
+      <div className="icons">
         <input {...getInputProps()} />
-        <div className='flex flex-col gap-4'>
-          <button
-            type='button'
-            onClick={open}
-            className='hover:cursor-pointer hover:underline text-white rounded-full bg-gradient-to-r from-blue-500 to-violet-600 text-bold px-6 py-4'
-          >
-            Subir imagen
-          </button>
-          <strong> O arrastra una imagen.</strong>
-        </div>
+        <button
+          className="btn stroke-primary hover:stroke-tertiary transition"
+          onClick={open}
+        >
+          <Paperclip size="size-6" />
+        </button>
       </div>
     </section>
   )
