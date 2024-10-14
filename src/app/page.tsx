b717674d2733d4413ce7033a421d2593d951c923
ValeHernandezz <valehernandezmodino@gmail.com>
2024-10-14 19:02:16 -0300

    Style: Cambio de eslogan

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,10 @@ import FormImage from '@/components/dropzone/FormImage'
 export default function Home() {
   return (
     <>
-      <main className='flex flex-col lg:flex-row w-full items-center justify-center h-[40vh]'>
+      <main className='flex flex-col lg:flex-row w-full items-center justify-center gap-x-16 h-[40vh]'>
         <div className='pb-5 lg:p-0'>
           <h1 className='md:max-w-[400px] text-center lg:text-left text-white'>
-            Un toque <span className='text-gradient'>sobrenatural</span> a tus
+            Dale un toque <span className='text-gradient'>sobrenatural</span> a tus
             recuerdos
           </h1>
         </div>
