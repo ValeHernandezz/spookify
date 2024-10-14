63c4b4156efcd67635e4fb30e6dc84f82360ca72
nicomelendez <nicomelendez10@gmail.com>
2024-10-13 23:13:45 -0300

    Add: Componente herramientas y estilado

--- src/app/(routes)/editar/page.tsx
+++ src/app/(routes)/editar/page.tsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import ViewImage from '@/components/image/ViewImage'
 import ChangeImage from '@/components/image/ChangeImage'
+import ListOfTools from '@/components/tools/ListOfTools'
 
 export default function EditorPage() {
   return (
     <main className='text-center text-white'>
       <h2 className='text-3xl font-bold'>Edita tu imagen</h2>
-      <section>
-        <ChangeImage/>
-        <ViewImage />
+      <section className='flex w-full justify-center pt-10'>
+        <ListOfTools />
+        <div className='flex flex-col'>
+          <ChangeImage />
+          <ViewImage />
+        </div>
       </section>
     </main>
   )
