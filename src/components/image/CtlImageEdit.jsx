f1dc892ef6ab1c45e61b8d837bafc28c7aea1cef
nicomelendez <nicomelendez10@gmail.com>
2024-10-13 23:14:22 -0300

    Add: Logica para mostrar la imagen editada

--- src/components/image/CtlImageEdit.jsx
+++ src/components/image/CtlImageEdit.jsx
@@ -1,23 +1,33 @@
 'use client'
-import { getCldImageUrl } from 'next-cloudinary'
 import Image from 'next/image'
 import React from 'react'
+import useEditor from '@/store/Providers'
+import Loader from '@/components/utils/Loader'
 
-export default function CtlImageEdit({ publicId }) {
-  const url = getCldImageUrl({
-    src: publicId,
-    replaceBackground: 'Add scary ghosts to the background',
-  })
+export default function CtlImageEdit() {
+  const { image, loading } = useEditor()
+
+  const url = image.transformedUrl || image.originalUrl
+  
+  if (loading) return <Loader />
+
+  if (!url) {
+    return (
+      <div>
+        No se ha aplicado ningun efecto. Por favor, selecciona una herramienta.
+      </div>
+    )
+  }
 
   return (
-    <div className='flex justify-center py-5'>
+    <div className='flex justify-center py-5 max-w-[600px]'>
       <Image
         id='preview'
         src={url}
         width={1000}
         height={1000}
         priority={true}
-        alt='iamgen'
+        alt='imagen editada'
       />
     </div>
   )
