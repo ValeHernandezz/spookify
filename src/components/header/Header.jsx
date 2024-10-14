ed1c4652f9a379ee97965ffb833377b37765fee5
ValeHernandezz <valehernandezmodino@gmail.com>
2024-10-14 19:02:44 -0300

    Style: Cambio de color y tamaño del header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,8 +4,8 @@ import Ghost from '@/components/icons/Ghost'
 export default function Header() {
   return (
     <header className="w-full p-10">
-      <h2 className="flex text-left items-center gap-x-2 text-2xl font-bold text-gradient">
-        <Ghost size="w-12" /> Spookify
+      <h2 className="flex text-left items-center gap-x-3 text-3xl font-bold text-gradient">
+        <Ghost size="w-10" /> Spookify
       </h2>
     </header>
   )
