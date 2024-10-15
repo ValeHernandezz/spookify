c8dcb7e9b7b8f3ff104265958325143b19fa9c32
nicomelendez <nicomelendez10@gmail.com>
2024-10-15 20:19:04 -0300

    Add: Iconos nuevos

---
+++ src/components/icons/Sparkles.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function Sparkles() {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      width='24'
+      height='24'
+      fill='none'
+      stroke='currentColor'
+      viewBox='0 0 24 24'
+      strokeWidth='2'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+      className='size-5'
+    >
+      <path d='M0 0h24v24H0z' stroke='none' />
+      <path d='M16 18a2 2 0 0 1 2 2 2 2 0 0 1 2-2 2 2 0 0 1-2-2 2 2 0 0 1-2 2zm0-12a2 2 0 0 1 2 2 2 2 0 0 1 2-2 2 2 0 0 1-2-2 2 2 0 0 1-2 2zM9 18a6 6 0 0 1 6-6 6 6 0 0 1-6-6 6 6 0 0 1-6 6 6 6 0 0 1 6 6z' />
+    </svg>
+  )
+}
