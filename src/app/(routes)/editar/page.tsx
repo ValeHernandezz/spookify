c4652bac8e462f2cadce3afe138b6f6d81e7a47b
nicomelendez <nicomelendez10@gmail.com>
2024-10-14 23:17:51 -0300

    Fix: Estilado de editor

--- src/app/(routes)/editar/page.tsx
+++ src/app/(routes)/editar/page.tsx
@@ -2,16 +2,25 @@ import React from 'react'
 import ViewImage from '@/components/image/ViewImage'
 import ChangeImage from '@/components/image/ChangeImage'
 import ListOfTools from '@/components/tools/ListOfTools'
+import Background from '@/components/utils/Background'
 
 export default function EditorPage() {
   return (
-    <main className='text-center text-white'>
-      <h2 className='text-3xl font-bold'>Edita tu imagen</h2>
-      <section className='flex w-full justify-center pt-10'>
-        <ListOfTools />
-        <div className='flex flex-col'>
-          <ChangeImage />
+    <main className='min-h-[100vh]'>
+      <Background z={true} />
+      <section className='flex'>
+        <div className='flex h-screen flex-col bg-white w-[400px]'>
+          <div className='px-4'>
+            <h2 className='text-3xl font-bold text-black pt-5'>
+              Edita tu imagen
+            </h2>
+            <ListOfTools />
+          </div>
+        </div>
+        <div className='flex bg-slate-200 flex-col w-full'>
           <ViewImage />
+
+          <ChangeImage />
         </div>
       </section>
     </main>
