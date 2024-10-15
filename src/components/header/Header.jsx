cdf6ec08a33cf53c7b3a0a0e1bf10b6dae24e306
nicomelendez <nicomelendez10@gmail.com>
2024-10-14 23:18:25 -0300

    Fix: Arreglos de estilos

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Ghost from '@/components/icons/Ghost'
+import Link from 'next/link'
 
 export default function Header() {
   return (
-    <header className="w-full p-10">
-      <h2 className="flex text-left items-center gap-x-3 text-3xl font-bold text-gradient">
-        <Ghost size="w-10" /> Spookify
-      </h2>
+    <header className='w-full p-10'>
+      <Link
+        href='/'
+        className='flex text-left items-center gap-x-3 text-3xl font-bold text-gradient'
+      >
+        <Ghost size='w-10' /> Spookify
+      </Link>
     </header>
   )
 }
