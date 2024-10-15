2a7b22fc83c16332c2327c12d8fb2494397f2e1e
nicomelendez <nicomelendez10@gmail.com>
2024-10-15 20:24:28 -0300

    Fix: Propiedad strokeWidth

--- src/components/icons/InputSpark.jsx
+++ src/components/icons/InputSpark.jsx
@@ -9,9 +9,9 @@ export default function InputSpark() {
       fill='none'
       viewBox='0 0 24 24'
       stroke='currentColor'
-      stroke-width='2'
-      stroke-linecap='round'
-      stroke-linejoin='round'
+      strokeWidth='2'
+      strokeLinecap='round'
+      strokeLinejoin='round'
       className='size-5'
     >
       <path d='M0 0h24v24H0z' stroke='none' />
