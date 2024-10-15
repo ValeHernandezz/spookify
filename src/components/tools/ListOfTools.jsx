ec037fc49fa6f05eb2aac20d294773051009e161
nicomelendez <nicomelendez10@gmail.com>
2024-10-14 23:19:42 -0300

    Fix: Arreglo de estilado

--- src/components/tools/ListOfTools.jsx
+++ src/components/tools/ListOfTools.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 import useTransform from '@/hooks/useTransform'
 import useEditor from '@/store/Providers'
 import { tools } from '@/lib'
-
+import { ToolCategory } from '@/lib/types'
+import Trash from '@/components/icons/Trash'
+import Undo from '@/components/icons/Undo'
 export default function ListOfTools() {
   const { image, changeImage } = useEditor()
   const { transformImage } = useTransform()
@@ -95,32 +97,64 @@ export default function ListOfTools() {
 
   return (
     <div>
-      <ul className='flex flex-col gap-y-5 px-10'>
-        {tools.map(({ id, title, transformations }) => (
-          <button
-            key={id}
-            className='p-4 bg-secondary rounded-lg'
-            onClick={() => handleTransform(transformations)}
+      <ul className='mt-6 space-y-1'>
+        {Object.values(ToolCategory).map((category) => (
+          <details
+            className='group [&_summary::-webkit-details-marker]:hidden'
+            key={category}
           >
-            {title}
-          </button>
+            <summary className='flex cursor-pointer items-center justify-between rounded-lg px-4 py-2 text-gray-500 hover:bg-gray-100 hover:text-gray-700'>
+              <span className='text-sm font-medium'>{category}</span>
+              <span className='shrink-0 transition duration-300 group-open:-rotate-180'>
+                <svg
+                  xmlns='http://www.w3.org/2000/svg'
+                  className='size-5'
+                  viewBox='0 0 20 20'
+                  fill='currentColor'
+                >
+                  <path
+                    fillRule='evenodd'
+                    d='M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z'
+                    clipRule='evenodd'
+                  />
+                </svg>
+              </span>
+            </summary>
+
+            <ul className='mt-2 space-y-1 px-4'>
+              {tools
+                .filter((tool) => tool.category === category)
+                .map((tool) => (
+                  <li key={tool.id}>
+                    <button
+                      className='w-full text-left rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700'
+                      onClick={() => handleTransform(tool.transformations)}
+                    >
+                      {tool.title}
+                    </button>
+                  </li>
+                ))}
+            </ul>
+          </details>
         ))}
       </ul>
 
-      <div className='flex gap-5 mt-5'>
+      <div className='flex justify-center gap-5 mt-10'>
         <button
-          className='p-4 bg-red-500 text-white rounded-lg'
+          className='flex items-center gap-x-2 font-bold p-2 text-sm bg-red-500 text-white rounded-lg'
           onClick={handleUndo}
           disabled={!(image.appliedTransformations || []).length}
         >
-          Deshacer último efecto
+          <Undo />
+          Deshacer
         </button>
         <button
-          className='p-4 bg-red-800 text-white rounded-lg'
+          className='flex items-center gap-x-2 font-bold p-2 text-sm bg-red-800 text-white rounded-lg'
           onClick={handleReset}
           disabled={!(image.appliedTransformations || []).length}
         >
-          Borrar todos los efectos
+          <Trash />
+          Limpiar
         </button>
       </div>
     </div>
