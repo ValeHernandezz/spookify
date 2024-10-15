12b1816f8fc6326107ccb5f15a5a5895ffca51cf
nicomelendez <nicomelendez10@gmail.com>
2024-10-14 23:20:00 -0300

    Fix: Arreglo para controlar la capa z

--- src/components/utils/Background.jsx
+++ src/components/utils/Background.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 
-export default function Background() {
+export default function Background({ z }) {
   return (
-    <div className='fixed top-0 z-[-50] h-[130vh] w-[130vw] bg-gradient-to-b from-zinc-900 to-zinc-800'></div>
+    <div
+      className={`${
+        z ? 'z-[-1]' : 'z-[-50]'
+      } fixed inset-0 h-[130vh] w-[130vw] bg-gradient-to-b from-zinc-900 to-zinc-800`}
+    ></div>
   )
 }
