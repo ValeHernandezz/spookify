98e175d84c146207fd153266d5747a9f4aefc156
nicomelendez <nicomelendez10@gmail.com>
2024-10-14 23:18:01 -0300

    Fix: Arreglos de estilos

--- src/components/dropzone/FormImage.jsx
+++ src/components/dropzone/FormImage.jsx
@@ -6,20 +6,21 @@ import { useRouter } from 'next/navigation'
 import Paperclip from '@/components/icons/PaperClip'
 
 export default function FormImage() {
-  const { changeImage } = useEditor()
+  const { changeImage, loading, changeLoading } = useEditor()
   const router = useRouter()
 
   const { getRootProps, getInputProps, open } = useDropzone({
     noClick: true,
     accept: {
-      'image/*': []
+      'image/*': [],
     },
     onDrop: (acceptedFiles) => {
       handleDrop(acceptedFiles)
-    }
+    },
   })
 
   const handleDrop = async (acceptedFiles) => {
+    changeLoading(true)
     const formData = new FormData()
 
     acceptedFiles.forEach((file) => {
@@ -28,28 +29,38 @@ export default function FormImage() {
 
     const response = await fetch('/api/upload', {
       method: 'POST',
-      body: formData
+      body: formData,
     })
 
     const { data } = await response.json()
 
     changeImage(data)
+    changeLoading(false)
     router.push('/editar')
   }
 
   return (
-    <section {...getRootProps({ className: 'dropzone' })} className="card">
-      <div className="text">
-        <span className="mb-5">¡Sube una imagen!</span>
-        <p className="subtitle">O arrástrala</p>
+    <section {...getRootProps({ className: 'dropzone' })} className='card'>
+      <div className='text'>
+        <span className='mb-5'>¡Sube una imagen!</span>
+        <p className='subtitle'>O arrástrala</p>
       </div>
-      <div className="icons">
+      <div className='icons'>
         <input {...getInputProps()} />
         <button
-          className="btn stroke-primary hover:stroke-tertiary transition"
+          className='btn stroke-primary hover:stroke-tertiary transition hover:text-white'
           onClick={open}
+          disabled={loading}
         >
-          <Paperclip size="size-6" />
+          {loading ? (
+            <>
+              <div class='loaderCircle mr-2'></div> Subiendo
+            </>
+          ) : (
+            <>
+              <Paperclip size='size-6 mr-2' /> Subir imagen
+            </>
+          )}
         </button>
       </div>
     </section>
