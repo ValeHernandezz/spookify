66e8d5f65c30c4a3e9abae2d1823ca94651e669c
ValeHernandezz <valehernandezmodino@gmail.com>
2024-10-16 22:24:44 -0300

    Add: Links a secciones

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,15 +2,30 @@ import React from 'react'
 import Ghost from '@/components/icons/Ghost'
 import Link from 'next/link'
 
+import { sections } from '@/lib'
+
 export default function Header() {
   return (
-    <header className='w-full p-10'>
-      <Link
-        href='/'
-        className='flex text-left items-center gap-x-3 text-3xl font-bold text-gradient'
-      >
-        <Ghost size='w-10' /> Spookify
-      </Link>
+    <header className="w-full p-10 flex items-center justify-between" id='inicio'>
+      <div>
+        <Link
+          href="/"
+          className="flex text-left items-center gap-x-3 text-3xl font-bold text-gradient"
+        >
+          <Ghost size="w-10" /> Spookify
+        </Link>
+      </div>
+      <nav>
+        <ul className="flex gap-8 text-slate-200">
+          {sections.map(({ id, name, url }) => {
+            return (
+              <li className="text-lg hover:bg-slate-200/10 py-1 px-2 transition duration-300 rounded-md" key={id}>
+                <a href={url}>{name}</a>
+              </li>
+            )
+          })}
+        </ul>
+      </nav>
     </header>
   )
 }
