040e12e1bf12fc17fa4659ee0d4c0b93e2ae1161
nicomelendez <nicomelendez10@gmail.com>
2024-10-19 16:06:48 -0300

    Add: funcionalidad de descargar imagen

--- src/components/image/ChangeImage.jsx
+++ src/components/image/ChangeImage.jsx
@@ -10,7 +10,21 @@ import Share from '@/components/icons/Share'
 import Button from '@/components/utils/Button'
 
 export default function ChangeImage() {
-  const { changeViewImage } = useEditor()
+  const { changeViewImage, image } = useEditor()
+
+  const downloadImage = async () => {
+    const response = await fetch(image.transformedUrl)
+    const blob = await response.blob()
+    const url = window.URL.createObjectURL(blob)
+    const a = document.createElement('a')
+    a.href = url
+    a.download = `${image.original_filename}.png`
+    document.body.appendChild(a)
+    a.click()
+    document.body.removeChild(a)
+    window.URL.revokeObjectURL(url)
+  }
+
   return (
     <div className='flex flex-col gap-y-5 lg:gap-y-0 lg:flex-row items-center lg:justify-between w-full max-w-[880px] lg:px-20 mx-auto'>
       <div className='flex items-center gap-5 xl:gap-10'>
@@ -40,7 +54,7 @@ export default function ChangeImage() {
       </div>
 
       <div className='flex items-center gap-5 xl:gap-10'>
-        <Button color='bg-primary' title='Descargar' onClick={() => {}}>
+        <Button color='bg-primary' title='Descargar' onClick={downloadImage}>
           <Download size='size-4 xl:size-5' />
         </Button>
 
