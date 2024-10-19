6563c0c63c3436a1d3aef2ebc1aadc96cc7e2bde
ValeHernandezz <valehernandezmodino@gmail.com>
2024-10-18 22:35:33 -0300

    Style: Mejoras en responsive de Footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -7,23 +7,23 @@ import { sections } from '@/lib'
 
 export default function Footer() {
   return (
-    <footer className='w-full p-10 border-t border-slate-200/50'>
-      <div className='mx-auto px-4 sm:px-6 lg:px-0'>
-        <div className='w-full mx-auto'>
+    <footer className="w-full p-10 border-t border-slate-200/50">
+      <div className="mx-auto px-4 sm:px-6 lg:px-0">
+        <div className="w-full mx-auto">
           <Link
-            href='#inicio'
-            className='flex justify-center items-center gap-x-3 text-3xl font-bold text-gradient'
+            href="#inicio"
+            className="flex justify-center items-center gap-x-3 text-3xl font-bold text-gradient"
           >
-            <Ghost size='w-10' /> Spookify
+            <Ghost size="w-10" /> Spookify
           </Link>
 
-          <ul className='text-lg flex items-center justify-center flex-col gap-7 md:flex-row md:gap-12 transition-all duration-500 py-16 '>
+          <ul className="text-lg flex items-center justify-center flex-col gap-4 md:gap-12 md:flex-row transition-all duration-500 py-16 ">
             {sections.map(({ id, name, url }) => {
               return (
                 <li key={id}>
                   <a
                     href={url}
-                    className='text-slate-400 hover:text-primary transition duration-500'
+                    className="text-slate-400 hover:text-primary transition duration-500"
                   >
                     {name}
                   </a>
@@ -32,56 +32,56 @@ export default function Footer() {
             })}
           </ul>
 
-          <div className='flex space-x-12 justify-center items-center mb-10'>
+          <div className="flex flex-col md:flex-row gap-4 md:gap-12 justify-center items-center mb-16 md:mb-10">
             <a
-              target='_blank'
-              rel='noreferrer noopener'
-              href='https://github.com/nicomelendez'
-              className='flex items-center gap-3 text-slate-200 transition-all duration-300 hover:text-primary opacity-85 hover:opacity-100'
+              target="_blank"
+              rel="noreferrer noopener"
+              href="https://github.com/nicomelendez"
+              className="flex items-center gap-3 text-slate-200 transition-all duration-300 hover:text-primary opacity-85 hover:opacity-100"
             >
               <img
-                className='size-9 rounded-full'
-                src='/profiles/nicomelendez-profile.webp'
-                alt='Foto perfil Valentina Hernández'
+                className="size-9 rounded-full"
+                src="/profiles/nicomelendez-profile.webp"
+                alt="Foto perfil Valentina Hernández"
               />
-              <small className='text-lg'>nicomelendez</small>
+              <small className="text-lg">nicomelendez</small>
             </a>
             <a
-              target='_blank'
-              rel='noreferrer noopener'
-              href='https://github.com/ValeHernandezz'
-              className='flex items-center gap-3 text-slate-200 transition-all duration-300 hover:text-primary opacity-85 hover:opacity-100'
+              target="_blank"
+              rel="noreferrer noopener"
+              href="https://github.com/ValeHernandezz"
+              className="flex items-center gap-3 text-slate-200 transition-all duration-300 hover:text-primary opacity-85 hover:opacity-100"
             >
               <img
-                className='size-9 rounded-full'
-                src='/profiles/valehernandezz-profile.webp'
-                alt='Foto perfil Valentina Hernández'
+                className="size-9 rounded-full"
+                src="/profiles/valehernandezz-profile.webp"
+                alt="Foto perfil Valentina Hernández"
               />
-              <small className='text-lg'>valehernandezz</small>
+              <small className="text-lg">valehernandezz</small>
             </a>
           </div>
 
-          <div className='grid grid-cols-1 md:grid-cols-3 gap-4 md:gap-0'>
-            <div className='flex justify-start items-center gap-3 text-slate-200'>
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-4 md:gap-0">
+            <div className="flex justify-center md:justify-start items-center gap-3 text-slate-200">
               <p>Hecho con</p>
               <a
-                target='_blank'
-                rel='noreferrer noopener'
-                href='https://cloudinary.com/'
+                target="_blank"
+                rel="noreferrer noopener"
+                href="https://cloudinary.com/"
               >
-                <Cloudinary className='h-8 text-[#3448C5] hover:text-[#3448C5]/80 transition duration-300' />
+                <Cloudinary className="h-8 text-[#3448C5] hover:text-[#3448C5]/80 transition duration-300" />
               </a>
             </div>
-            <span className='flex justify-center items-center text-base text-slate-500 text-center'>
-              ©<a href='#inicio'>Spookify</a> 2024 | #MoviendoLasManitas
+            <span className="flex justify-center md:justify-center items-center text-base text-slate-500 text-center">
+              ©<a href="#inicio">Spookify</a> 2024 | #MoviendoLasManitas
             </span>
             <a
-              target='_blank'
-              rel='noreferrer noopener'
-              href='https://github.com/spooky-ai-hackathon/spookify'
-              className='flex justify-end items-center text-slate-200 transition-all duration-500 hover:text-primary '
+              target="_blank"
+              rel="noreferrer noopener"
+              href="https://github.com/spooky-ai-hackathon/spookify"
+              className="flex justify-center md:justify-end items-center text-slate-200 transition-all duration-500 hover:text-primary "
             >
-              <GitHub size='size-9' />
+              <GitHub size="size-9" />
             </a>
           </div>
         </div>
