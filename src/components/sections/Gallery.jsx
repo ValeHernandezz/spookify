ee51e372927fb18ce34c736f6b39f4fe31b601aa
ValeHernandezz <valehernandezmodino@gmail.com>
2024-10-18 22:28:21 -0300

    Change: Refactorización de componente galería

--- src/components/sections/Gallery.jsx
+++ src/components/sections/Gallery.jsx
@@ -3,19 +3,29 @@ import { images } from '@/lib'
 
 export default function Gallery() {
   return (
-    <ul className='grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-10 text-slate-200 place-items-center max-w-[700px] xl:max-w-[1400px] mx-auto'>
-      {images.map(({ id, title, image }) => {
-        return (
-          <li key={id} className='hover:scale-105 cursor-crosshair transition'>
-            <img
-              className='rounded-3xl mb-4 max-w-[300px]'
-              src={image}
-              alt={`Imagen de ${title}`}
-            />
-            <span className='flex justify-center text-xl'>{title}</span>
-          </li>
-        )
-      })}
-    </ul>
+    <section id="examples" className="pt-20 pb-20 md:pb-40">
+      <h2 className="text-4xl font-bold text-center text-white mb-16 w-[15ch] md:w-auto mx-auto">
+        ¡Transforma tus imágenes en{' '}
+        <span className="text-gradient">pesadillas</span>!
+      </h2>
+
+      <ul className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-10 text-slate-200 place-items-center max-w-[700px] xl:max-w-[1400px] mx-auto">
+        {images.map(({ id, title, image }) => {
+          return (
+            <li
+              key={id}
+              className="hover:scale-105 cursor-crosshair transition"
+            >
+              <img
+                className="rounded-3xl mb-4 max-w-[300px]"
+                src={image}
+                alt={`Imagen de ${title}`}
+              />
+              <span className="flex justify-center text-xl">{title}</span>
+            </li>
+          )
+        })}
+      </ul>
+    </section>
   )
 }
