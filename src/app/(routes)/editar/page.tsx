e0fda586bebd49366226afeb3c040c66f5c22f43
nicomelendez <nicomelendez10@gmail.com>
2024-10-19 16:48:11 -0300

    Fix: Uso de etiqueta aside

--- src/app/(routes)/editar/page.tsx
+++ src/app/(routes)/editar/page.tsx
@@ -10,12 +10,12 @@ export default function EditorPage() {
       <Background z={true} />
       <section className='grid grid-cols-[350px_1fr]'>
         <div className='flex min-h-screen flex-col bg-white'>
-          <div className='px-4'>
+          <aside className='px-4'>
             <h2 className='text-2xl xl:text-3xl text-center font-bold text-black pt-5'>
               Edita tu imagen
             </h2>
             <ListOfTools />
-          </div>
+          </aside>
         </div>
         <div className='flex bg-slate-200 flex-col w-full'>
           <ViewImage />
