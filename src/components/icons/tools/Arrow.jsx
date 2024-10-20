d22abefe7b3b016148ecf253fcef34124634ed99
ValeHernandezz <valehernandezmodino@gmail.com>
2024-10-20 03:46:40 -0300

    Add: Nuevos iconos sección Herramientas

--- src/components/icons/tools/Arrow.jsx
+++ src/components/icons/tools/Arrow.jsx
@@ -3,7 +3,12 @@ import React from 'react'
 export default function Arrow({ className }) {
   return (
     <svg className={className} viewBox="0 0 24 24">
-      <path fill="currentColor" d="M8 19V5l11 7z" />
+      <path
+        fill="currentColor"
+        fill-rule="evenodd"
+        d="M1 12C1 5.925 5.925 1 12 1s11 4.925 11 11s-4.925 11-11 11S1 18.075 1 12m8.528-4.882a1 1 0 0 1 1.027.05l6 4a1 1 0 0 1 0 1.664l-6 4A1 1 0 0 1 9 16V8a1 1 0 0 1 .528-.882"
+        clip-rule="evenodd"
+      />
     </svg>
   )
 }
