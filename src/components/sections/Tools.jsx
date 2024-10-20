7e87271ac3bd2d7aebee7c4bbd1a5dda70a53a0a
ValeHernandezz <valehernandezmodino@gmail.com>
2024-10-20 03:49:27 -0300

    Change: Texto de la UI de Herramientas y mapeado de los linksTools

--- src/components/sections/Tools.jsx
+++ src/components/sections/Tools.jsx
@@ -1,47 +1,51 @@
 import React from 'react'
-import Arrow from '@/components/icons/tools/Arrow'
+import { toolsLinks } from '@/lib'
 
 export default function Tools() {
   return (
-    <section id='tools' className="text-slate-200 text-[17px] pt-10 md:pt-20 pb-60 grid xl:grid-cols-[650px_1fr] max-w-[700px] xl:max-w-[1400px] mx-auto gap-10 place-items-center">
-      <div className="px-6 md:px-0">
+    <section
+      id="tools"
+      className="text-slate-200 text-[17px] pt-10 md:pt-20 pb-60 grid xl:grid-cols-[650px_1fr] max-w-[700px] xl:max-w-[1700px] mx-auto gap-10 place-items-center px-6 md:px-0"
+    >
+      <div>
         <img src="/tools/tools.webp" alt="Imagen de herramientas" />
       </div>
-      <aside className='px-6 md:px-0'>
-        <h2 className="text-4xl text-center md:text-left font-bold mb-8 w-[15ch] md:w-auto mx-auto">
-          Se potencia con <span className="text-gradient">Cloudinary</span> y sus herramientas
+
+      <aside>
+        <h2 className="text-4xl text-center xl:text-left font-bold mb-8 w-[15ch] md:w-[25ch] mx-auto xl:mx-0">
+          Haz realidad tus monstruosidades con{' '}
+          <span className="text-gradient">Cloudinary</span> y{' '}
+          <span className="text-gradient">Perplexity</span>
         </h2>
-        <p className="mb-8 text-pretty md:text-balance">
-          Hemos integrado la API de Cloudinary y sus potentes herramientas para
-          transformar y optimizar tus imágenes de manera eficiente. Desde la
-          manipulación de imágenes hasta la generación de efectos únicos, puedes
-          aprovechar todo el poder de Cloudinary para llevar tu creatividad a un
-          nuevo nivel.
+
+        <p className="mb-8 text-pretty">
+          Edita tus imágenes al estilo Halloween de forma rápida y precisa con
+          la API de Cloudinary. Puedes optar por uno de nuestros estilos
+          predeterminados o personalizar tu creación con una idea propia, y
+          Perplexity se encargará de que tu descripción se refleje tal como la
+          imaginaste.
         </p>
-        <div className="flex flex-col gap-3 text-primary">
-          <div className="w-max">
-            <a
-              className="flex items-center transition duration-300 hover:text-tertiary"
-              href="https://next.cloudinary.dev/cldimage/examples"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <Arrow className="size-5 mr-2" />
-              Documentación de la API de Cloudinary
-            </a>
-          </div>
-          <div className="w-max">
-            <a
-              className="flex items-center transition duration-300 hover:text-tertiary"
-              href="https://cloudinary.com/demos"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <Arrow className="size-5 mr-2" />
-              Prueba las demo de Cloudinary aquí
-            </a>
-          </div>
-        </div>
+
+        <ul className="flex flex-col gap-3 text-primary">
+          {toolsLinks.map(({ id, title, href, icon: Icon }) => {
+            return (
+              <li key={id} className="w-max ">
+                <a
+                  className="flex items-center transition duration-500 hover:text-tertiary"
+                  href={href}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <Icon className="size-5 mr-2" />
+                  <div className="group relative w-max">
+                    {title}
+                    <span class="absolute -bottom-1 left-0 w-0 transition-all h-0.5 bg-primary group-hover:w-full"></span>
+                  </div>
+                </a>
+              </li>
+            )
+          })}
+        </ul>
       </aside>
     </section>
   )
