9e801983ff53960cd6937534e3bdcd7deef1efdc
nicomelendez <nicomelendez10@gmail.com>
2024-10-20 03:02:08 -0300

    Add: Metodos para compartir y descargar imagen

--- src/components/image/ChangeImage.jsx
+++ src/components/image/ChangeImage.jsx
@@ -1,5 +1,5 @@
 'use client'
-import React from 'react'
+import React, { useState } from 'react'
 import useEditor from '@/store/Providers'
 import { ViewImageStateEnum } from '@/lib/types'
 import Photo from '@/components/icons/Photo'
@@ -11,6 +11,8 @@ import Button from '@/components/utils/Button'
 
 export default function ChangeImage() {
   const { changeViewImage, image } = useEditor()
+  const [copied, setCopied] = useState(false)
+  const [loading, setLoading] = useState(false)
 
   const downloadImage = async () => {
     const response = await fetch(image.transformedUrl)
@@ -23,6 +25,32 @@ export default function ChangeImage() {
     a.click()
     document.body.removeChild(a)
     window.URL.revokeObjectURL(url)
+  }
+
+  const sharedImage = async () => {
+    setLoading(true)
+    const imageResponse = await fetch(image.transformedUrl)
+    const blob = await imageResponse.blob()
+    const formData = new FormData()
+    formData.append('file', blob, 'image.jpg') // El tercer parámetro es el nombre del archivo
+
+    const response = await fetch('/api/shared', {
+      method: 'POST',
+      body: formData,
+    })
+
+    const { data } = await response.json()
+
+    const urlToShared = `${process.env.NEXT_PUBLIC_URL_FRONTEND}/${data.id}`
+
+    setLoading(false)
+    setCopied(true)
+
+    navigator.clipboard.writeText(urlToShared)
+
+    setTimeout(function () {
+      setCopied(false)
+    }, 1300)
   }
 
   return (
@@ -58,8 +86,33 @@ export default function ChangeImage() {
           <Download size='size-4 xl:size-5' />
         </Button>
 
-        <Button color='bg-primary' title='Compartir' onClick={() => {}}>
-          <Share size='size-4 xl:size-5' />
+        <Button
+          color='bg-primary'
+          title={!loading ? (copied ? 'Copiado' : 'Compartir') : 'Generando'}
+          onClick={sharedImage}
+        >
+          {!loading ? (
+            copied ? (
+              <svg
+                viewBox='0 0 24 24'
+                fill='none'
+                stroke='currentColor'
+                strokeWidth='2'
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                className='size-5'
+              >
+                <path d='M0 0h24v24H0z' stroke='none' />
+                <path d='M0 0h24v24H0z' stroke='none' />
+                <path d='M7 9.667A2.667 2.667 0 0 1 9.667 7h8.666A2.667 2.667 0 0 1 21 9.667v8.666A2.667 2.667 0 0 1 18.333 21H9.667A2.667 2.667 0 0 1 7 18.333z' />
+                <path d='M4.012 16.737A2 2 0 0 1 3 15V5c0-1.1.9-2 2-2h10c.75 0 1.158.385 1.5 1M11 14l2 2 4-4' />
+              </svg>
+            ) : (
+              <Share size='size-4 xl:size-5' />
+            )
+          ) : (
+            <div className='loaderCircleSmall mr-2'></div>
+          )}
         </Button>
       </div>
     </div>
