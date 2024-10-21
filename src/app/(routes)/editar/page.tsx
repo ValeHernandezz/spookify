bcf6c88f658cff27593ed5e77056a743bcd84b7b
nicomelendez <nicomelendez10@gmail.com>
2024-10-20 22:29:00 -0300

    Styles: Arreglos para la version movil

--- src/app/(routes)/editar/page.tsx
+++ src/app/(routes)/editar/page.tsx
@@ -1,16 +1,34 @@
 'use client'
-import React from 'react'
+import React, { useState } from 'react'
 import ViewImage from '@/components/image/ViewImage'
 import ChangeImage from '@/components/image/ChangeImage'
 import ListOfTools from '@/components/tools/ListOfTools'
 import Background from '@/components/utils/Background'
 
 export default function EditorPage() {
+  const [isAsideVisible, setIsAsideVisible] = useState(false)
+
+  const toggleAside = () => {
+    setIsAsideVisible(!isAsideVisible)
+  }
+
   return (
     <main className='min-h-[100vh]'>
       <Background z={true} />
-      <section className='grid grid-cols-[350px_1fr]'>
-        <div className='flex min-h-screen flex-col bg-white'>
+
+      <button
+        className='block lg:hidden absolute top-36 right-5 w-max z-50 p-2 bg-gray-800 text-white rounded'
+        onClick={toggleAside}
+      >
+        {isAsideVisible ? 'Cerrar Menú' : 'Abrir Menú'}
+      </button>
+
+      <section className='grid grid-cols-1 lg:grid-cols-[350px_1fr]'>
+        <div
+          className={`flex min-h-screen flex-col bg-white transition-transform duration-300 ${
+            isAsideVisible ? 'translate-x-0' : '-translate-x-full'
+          } lg:translate-x-0 lg:block fixed lg:static z-40 w-64 lg:w-auto`}
+        >
           <aside className='px-4'>
             <h2 className='text-2xl xl:text-3xl text-center font-bold text-black pt-5'>
               Edita tu imagen
@@ -18,9 +36,9 @@ export default function EditorPage() {
             <ListOfTools />
           </aside>
         </div>
-        <div className='flex bg-slate-200 flex-col w-full pb-10'>
-          <ViewImage />
 
+        <div className='flex bg-slate-200 flex-col w-full pb-10 pt-[4.2rem] lg:pt-0'>
+          <ViewImage />
           <ChangeImage />
         </div>
       </section>
