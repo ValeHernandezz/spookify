8eedc2ae991b5fe572cc91d3a3f8d078d89b46dd
ValeHernandezz <valehernandezmodino@gmail.com>
2024-10-21 02:58:15 -0300

    Fix: Estilos y espacio utilizado por el loader "Transformando..."

--- src/components/image/ViewImage.jsx
+++ src/components/image/ViewImage.jsx
@@ -10,7 +10,12 @@ import { ViewImageStateEnum } from '@/lib/types'
 export default function ViewImage() {
   const { image, viewImage } = useEditor()
 
-  if (!image.public_id) return <Loader />
+  if (!image.public_id)
+    return (
+      <div className="h-[400px] flex items-center justify-center">
+        <Loader />
+      </div>
+    )
 
   if (viewImage === ViewImageStateEnum.ORIGINAL) return <CtlImage />
 
@@ -18,23 +23,3 @@ export default function ViewImage() {
 
   if (viewImage === ViewImageStateEnum.COMPARE) return <CtlImageCompare />
 }
-
-/* 'use client'
-import React from 'react'
-import ImageContainer from './ImageContainer'
-import Image from 'next/image'
-
-export default function ViewImage() {
-  return (
-    <ImageContainer>
-      <Image
-        className='w-full object-cover'
-        src='./background.webp'
-        width={1000}
-        height={700}
-        priority={true}
-        alt={`Imagen`}
-      />
-    </ImageContainer>
-  )
-} */
