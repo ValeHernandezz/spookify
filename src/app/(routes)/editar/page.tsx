4e18553ed3d9fdf136464f399d90b5326901bcc2
ValeHernandezz <valehernandezmodino@gmail.com>
2024-10-21 03:53:50 -0300

    Style: Cambios te colores y tamaños de componentes en Editar

--- src/app/(routes)/editar/page.tsx
+++ src/app/(routes)/editar/page.tsx
@@ -1,43 +1,43 @@
 'use client'
-import React, { useState } from 'react'
+import React from 'react'
 import ViewImage from '@/components/image/ViewImage'
 import ChangeImage from '@/components/image/ChangeImage'
 import ListOfTools from '@/components/tools/ListOfTools'
 import Background from '@/components/utils/Background'
+import useEditor from '@/store/Providers'
 
 export default function EditorPage() {
-  const [isAsideVisible, setIsAsideVisible] = useState(false)
-
-  const toggleAside = () => {
-    setIsAsideVisible(!isAsideVisible)
-  }
+  const { isAsideVisible, toggleAside } = useEditor()
 
   return (
-    <main className='min-h-[100vh]'>
+    <main className="min-h-[100vh] mb-32">
       <Background z={true} />
 
       <button
-        className='block lg:hidden absolute top-36 right-5 w-max z-50 p-2 bg-gray-800 text-white rounded'
+        className="block lg:hidden absolute top-36 right-5 w-max z-20 p-2 bg-gray-800 text-white rounded"
         onClick={toggleAside}
       >
         {isAsideVisible ? 'Cerrar Menú' : 'Abrir Menú'}
       </button>
 
-      <section className='grid grid-cols-1 lg:grid-cols-[350px_1fr]'>
+      <section className="grid grid-cols-1 lg:grid-cols-[350px_1fr] px-4 2xl:px-0">
         <div
-          className={`flex min-h-screen flex-col bg-white transition-transform duration-300 ${
-            isAsideVisible ? 'translate-x-0' : '-translate-x-full'
-          } lg:translate-x-0 lg:block fixed lg:static z-40 w-64 lg:w-auto`}
+          className={`flex lg:min-h-screen flex-col bg-[#18181b] transition-transform duration-300 rounded-lg lg:rounded-none lg:rounded-l-lg ${
+            isAsideVisible ? 'translate-x-0' : '-translate-x-[110%]'
+          } lg:translate-x-0 lg:block fixed lg:static z-10 w-64 lg:w-auto`}
         >
-          <aside className='px-4'>
-            <h2 className='text-2xl xl:text-3xl text-center font-bold text-black pt-5'>
+          <aside className="p-5 lg:pt-10">
+            <h2 className="text-2xl lg:text-3xl text-center font-bold text-slate-200">
               Edita tu imagen
             </h2>
+            <p className='text-sm xl:text-base text-slate-200/70 text-center mt-3 mb-6 mx-4 text-pretty'>
+              Usa imágenes con buena calidad para mejores resultados.
+            </p>
             <ListOfTools />
           </aside>
         </div>
 
-        <div className='flex bg-slate-200 flex-col w-full pb-10 pt-[4.2rem] lg:pt-0'>
+        <div className="flex bg-zinc-950 flex-col w-full pb-10 pt-[4.2rem] lg:pt-0 rounded-lg lg:rounded-none lg:rounded-r-lg">
           <ViewImage />
           <ChangeImage />
         </div>
