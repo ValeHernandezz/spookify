df81e72737c88d65b5ed0ce256eac4e219dd0e80
ValeHernandezz <valehernandezmodino@gmail.com>
2024-10-23 00:34:09 -0300

    Add: index para los iconos

---
+++ src/components/icons/index.ts
@@ -0,0 +1,85 @@
+import Alien from '@/icons/listOfTools/transform/Alien'
+import Arrow from '@/icons/tools/Arrow'
+import ArrowListOfTools from '@/icons/listOfTools/general/Arrow'
+import Background from '@/icons/listOfTools/general/Background'
+import Biohazard from '@/icons/listOfTools/background/Biohazard'
+import Chucky from '@/icons/listOfTools/costumes/Chucky'
+import Clothe from '@/icons/listOfTools/general/Clothe'
+import CloudinaryLogo from '@/icons/tools/Cloudinary'
+import CloudinaryWordmark from '@/icons/footer/Cloudinary'
+import Compare from '@/icons/viewImage/Compare'
+import Copy from '@/icons/viewImage/Copy'
+import CropZone from '@/icons/listOfTools/adjust/CropZone'
+import Devil from '@/icons/listOfTools/transform/Devil'
+import Download from '@/icons/viewImage/Download'
+import Edit from '@/icons/viewImage/Edit'
+import Expand from '@/icons/listOfTools/adjust/Expand'
+import FreddyKrueger from '@/icons/listOfTools/costumes/FreddyKrueger'
+import Ghost from '@/icons/header/Ghost'
+import GitHub from '@/icons/footer/GitHub'
+import Grave from '@/icons/listOfTools/background/Grave'
+import JasonVoorhees from '@/icons/listOfTools/overlay/JasonVoorhees'
+import Leatherface from '@/icons/listOfTools/overlay/Leatherface'
+import MagnifyingGlassPlus from '@/icons/shared/MagnifyingGlassPlus'
+import Menu from '@/icons/hamburger/Menu'
+import MichaelMyers from '@/icons/listOfTools/overlay/MichaelMyers'
+import MushroomCloud from '@/icons/listOfTools/background/MushroomCloud'
+import Overlay from '@/icons/listOfTools/general/Overlay'
+import PaperClip from '@/icons/dropdown/PaperClip'
+import Pennywise from '@/icons/listOfTools/overlay/Pennywise'
+import Perplexity from '@/icons/tools/Perplexity'
+import Photo from '@/icons/viewImage/Photo'
+import Rocket from '@/icons/shared/Rocket'
+import Ruler from '@/icons/listOfTools/general/Ruler'
+import Share from '@/icons/viewImage/Share'
+import Skeleton from '@/icons/listOfTools/transform/Skeleton'
+import Sparkles from '@/icons/listOfTools/general/Sparkles'
+import Trash from '@/icons/listOfTools/general/Trash'
+import Undo from '@/icons/listOfTools/general/Undo'
+import Wand from '@/icons/listOfTools/general/Wand'
+import X from '@/icons/hamburger/X'
+import Zombie from '@/icons/listOfTools/transform/Zombie'
+
+export {
+  Alien,
+  Arrow,
+  ArrowListOfTools,
+  Background,
+  Biohazard,
+  Chucky,
+  Clothe,
+  CloudinaryLogo,
+  CloudinaryWordmark,
+  Compare,
+  Copy,
+  CropZone,
+  Devil,
+  Download,
+  Edit,
+  Expand,
+  FreddyKrueger,
+  Ghost,
+  GitHub,
+  Grave,
+  JasonVoorhees,
+  Leatherface,
+  MagnifyingGlassPlus,
+  Menu,
+  MichaelMyers,
+  MushroomCloud,
+  Overlay,
+  PaperClip,
+  Pennywise,
+  Perplexity,
+  Photo,
+  Rocket,
+  Ruler,
+  Share,
+  Skeleton,
+  Sparkles,
+  Trash,
+  Undo,
+  Wand,
+  X,
+  Zombie
+}
