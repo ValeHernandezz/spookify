1ef933f4f87fa7ee1955ac19f985d4fe2691930e
nicomelendez <nicomelendez10@gmail.com>
2024-10-22 21:18:22 -0300

    Add: Variable para saber si hay que deshabilitarlo

--- src/components/utils/Button.tsx
+++ src/components/utils/Button.tsx
@@ -5,6 +5,7 @@ interface ButtonProps {
   title: string
   onClick: () => void
   children: React.ReactNode
+  disabled?: boolean
 }
 
 export default function Button({
@@ -12,11 +13,17 @@ export default function Button({
   title,
   onClick,
   children,
+  disabled,
 }: ButtonProps) {
   return (
     <button
-      className={`flex items-center gap-x-2 text-sm lg:text-base py-1.5 px-3 font-medium rounded-lg hover:opacity-85 transition duration-300 ${color}`}
+      className={`flex items-center gap-x-2 text-sm lg:text-base py-1.5 px-3 font-medium rounded-lg hover:opacity-85 transition duration-300 ${color} ${
+        disabled
+          ? 'cursor-not-allowed opacity-50'
+          : 'cursor-pointer hover:opacity-85'
+      }`}
       onClick={onClick}
+      disabled={disabled}
     >
       {children}
       {title}
