43dd19777f6ce175a9f1bfc8cbaa01eda264f53f
nicomelendez <nicomelendez10@gmail.com>
2024-10-29 13:37:44 -0300

    Remove: Provider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,13 @@
-'use client'
-import { Providers } from '@/store/Providers'
 import Gallery from '@/components/sections/Examples'
 import Hero from '@/sections/Hero'
 import Tools from '@/sections/Tools'
 
 export default function Home() {
   return (
-    <Providers>
-      <main>
-        <Hero />
-        <Gallery />
-        <Tools />
-      </main>
-    </Providers>
+    <main>
+      <Hero />
+      <Gallery />
+      <Tools />
+    </main>
   )
 }
