991cb14c229f990df8dc2377f72a39ea7a3f54b2
nicomelendez <nicomelendez10@gmail.com>
2024-10-30 11:20:35 -0300

    FIx: propiedad mal escrita

--- src/components/icons/shared/MagnifyingGlassPlus.jsx
+++ src/components/icons/shared/MagnifyingGlassPlus.jsx
@@ -8,7 +8,7 @@ export default function MagnifyingGlassPlus({ className }) {
         stroke="currentColor"
         strokeLinecap="round"
         strokeLinejoin="round"
-        strokewidth="2"
+        strokeWidth="2"
         d="M7 10h3m0 0h3m-3 0V7m0 3v3m5 2l6 6m-11-4a7 7 0 1 1 0-14a7 7 0 0 1 0 14"
       />
     </svg>
